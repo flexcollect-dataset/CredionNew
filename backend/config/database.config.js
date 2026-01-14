e3c29fe1da0beb59aecdb9fade6d443d2c1a69cc
mitalk11 <mitalk11@gmail.com>
2026-01-14 13:37:35 +1100

    Remove dataset access from coding file.

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        host: process.env.DB_HOST || 'localhost',
-        database: process.env.DB_NAME || 'FlexDataseterMaster',
-        username: process.env.DB_USER || 'FlexUser',
-        password: process.env.DB_PASS || 'Luffy123&&Lucky',
-        port: process.env.DB_PORT || 15432,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT,
         dialect: 'postgres',
         logging: console.log,
         pool: {
